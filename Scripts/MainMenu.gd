41f3ae056cb995b97a9feafa52573ae347ca859d
Dee <dee.covan@gmail.com>
2025-11-16 17:47:34 +0200

    Add README.MD

---
+++ Scripts/MainMenu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _process(_delta):
+	if Input.is_action_just_pressed('reload'):
+		get_tree().reload_current_scene()
+	if Input.is_action_just_pressed('screen'):
+		var mode := DisplayServer.window_get_mode()
+		var is_window: bool = mode != DisplayServer.WINDOW_MODE_FULLSCREEN
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN \
+			if is_window else DisplayServer.WINDOW_MODE_WINDOWED)
---
+++ edit.cmd
@@ -0,0 +1,1 @@
+start C:\Godot4\Godot_v4.5.1-stable_win64\Godot_v4.5.1-stable_win64.exe project.godot
