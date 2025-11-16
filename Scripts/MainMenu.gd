151c2d0c8d364c923c81fe6ce66c0a82d206354c
Dee <dee.covan@gmail.com>
2025-11-16 22:15:54 +0200

    Chains Web project

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,7 +1,8 @@
-extends Control
+extends Node2D
 
 func _process(_delta):
 	if Input.is_action_just_pressed('reload'):
+		print_debug('Reloading scene...')
 		get_tree().reload_current_scene()
 	if Input.is_action_just_pressed('screen'):
 		var mode := DisplayServer.window_get_mode()
